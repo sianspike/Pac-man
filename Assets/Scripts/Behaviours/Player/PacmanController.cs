fdd01316bf5a4b6b9d137b970df77a5d7c854029
Sian Pike <sian.pike@icloud.com>
2021-02-11 14:33:33 +0000

    pacman can now move left and right

--- Assets/Scripts/Behaviours/Player/PacmanController.cs
+++ Assets/Scripts/Behaviours/Player/PacmanController.cs
@@ -1,18 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PacmanController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float speed;
+    
+    private Rigidbody2D rigidBody;
+    private Vector2 inputDirection;
+    private Vector2 velocity;
+
+    private void Awake()
     {
-        
+        rigidBody = gameObject.GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Move(inputDirection);
+    }
+
+    public void Move(Vector2 direction)
+    {
+        velocity = rigidBody.velocity;
+        velocity.x = direction.x * speed;
+        rigidBody.velocity = velocity;
+    }
+
+    public void OnMove(InputValue input)
+    {
+        inputDirection = input.Get<Vector2>();
     }
 }
