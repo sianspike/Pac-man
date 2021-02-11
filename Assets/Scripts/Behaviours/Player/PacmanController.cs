a1d38e91363a869fd2d87a54b84cc671ea8fd68b
Sian Pike <sian.pike@icloud.com>
2021-02-11 17:52:44 +0000

    Pacman now collides with maze as expected, fixing movement speeds

--- Assets/Scripts/Behaviours/Player/PacmanController.cs
+++ Assets/Scripts/Behaviours/Player/PacmanController.cs
@@ -8,29 +8,41 @@ public class PacmanController : MonoBehaviour
 {
     [SerializeField] private float speed;
     
-    private Rigidbody2D rigidBody;
-    private Vector2 inputDirection;
-    private Vector2 velocity;
+    private Rigidbody2D _rigidBody;
+    private Vector2 _inputDirection;
+    private Vector2 _velocity;
 
     private void Awake()
     {
-        rigidBody = gameObject.GetComponent<Rigidbody2D>();
+        _rigidBody = gameObject.GetComponent<Rigidbody2D>();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        Move(inputDirection);
+
+        _rigidBody.isKinematic = true;
+        
+        if (_inputDirection != Vector2.zero)
+        {
+            _rigidBody.isKinematic = false;
+            Move(_inputDirection);
+        }
     }
 
-    public void Move(Vector2 direction)
+    private void Move(Vector2 direction)
     {
-        velocity = rigidBody.velocity;
-        velocity.x = direction.x * speed;
-        rigidBody.velocity = velocity;
+        _velocity = _rigidBody.velocity;
+        _velocity.x = direction.x * speed * Time.deltaTime;
+        _rigidBody.velocity = _velocity;
+        // var xForce = direction.x * speed * Time.deltaTime;
+        // var force = new Vector2(xForce, 0);
+        // _rigidBody.AddForce(force);
     }
 
-    public void OnMove(InputValue input)
+    public void OnMove(InputAction.CallbackContext input)
     {
-        inputDirection = input.Get<Vector2>();
+        if (!input.started) return;
+        _inputDirection = input.ReadValue<Vector2>();
+        Debug.Log("Button pressed");
     }
 }
