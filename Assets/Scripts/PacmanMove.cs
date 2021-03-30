606f906c08bdbbedc4d312343351b678639b47dd
Sian Pike <sian.pike@icloud.com>
2021-03-30 21:13:37 +0100

    pacman now eats pellets

--- Assets/Scripts/PacmanMove.cs
+++ Assets/Scripts/PacmanMove.cs
@@ -9,13 +9,17 @@ public class PacmanMove : MonoBehaviour
     private Vector2 _direction = Vector2.zero;
     private Vector2 _nextDirection;
     private Node _previousNode, _currentNode, _targetNode;
+    private GameBoard _gameBoard;
+    
     private Animator _animator;
     private SpriteRenderer _spriteRenderer;
 
+    //MOVING
     private void Start()
     {
         _animator = GetComponent<Animator>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
+        _gameBoard = GameObject.Find("game").GetComponent<GameBoard>();
         
         //pacman position
         var node = GetNodeAtPosition(transform.localPosition);
@@ -37,6 +41,7 @@ public class PacmanMove : MonoBehaviour
         Move();
         UpdateOrientation();
         UpdateAnimation();
+        ConsumePellet();
     }
 
     private void CheckInput()
@@ -61,7 +66,18 @@ public class PacmanMove : MonoBehaviour
 
     private void Move()
     {
-        if (_targetNode == _currentNode || _targetNode == null) return;
+        if (_targetNode == _currentNode || ReferenceEquals(_targetNode, null)) return;
+
+        //if pacman wants to move in opposite direction before node is reached
+        if (_nextDirection == _direction * -1)
+        {
+            _direction *= -1;
+
+            var temporaryNode = _targetNode;
+            
+            _targetNode = _previousNode;
+            _previousNode = temporaryNode;
+        }
         
         if (OvershotTarget())
         {
@@ -96,7 +112,78 @@ public class PacmanMove : MonoBehaviour
             transform.localPosition += (Vector3) (_direction * speed) * Time.deltaTime;
         }
     }
+    
+    private Node GetNodeAtPosition(Vector2 position)
+    {
+        var tile = _gameBoard.Board[(int) position.x, (int) position.y];
 
+        return tile != null ? tile.GetComponent<Node>() : null;
+    }
+    
+    private Node CanMove(Vector2 direction)
+    {
+        Node moveToNode = null;
+
+        //iterate over neighbours of pellet
+        for (var i = 0; i < _currentNode.neighbours.Length; i++)
+        {
+            //check if current direction of pacman is valid
+            if (_currentNode.validDirections[i] != direction) continue;
+            
+            moveToNode = _currentNode.neighbours[i];
+            
+            break;
+        }
+
+        return moveToNode;
+    }
+    
+    private void MoveToNode(Vector2 direction)
+    {
+        var moveToNode = CanMove(direction);
+
+        if (moveToNode == null) return;
+        
+        //change pacman's position if he is able to move
+        transform.localPosition = moveToNode.transform.position;
+        _currentNode = moveToNode;
+    }
+    
+    private void ChangePosition(Vector2 targetDirection)
+    {
+        if (targetDirection != _direction)
+        {
+            _nextDirection = targetDirection;
+        }
+
+        if (ReferenceEquals(_currentNode, null)) return;
+        
+        var moveToNode = CanMove(targetDirection);
+
+        if (ReferenceEquals(moveToNode, null)) return;
+            
+        _direction = targetDirection;
+        _targetNode = moveToNode;
+        _previousNode = _currentNode;
+        _currentNode = null;
+    }
+    
+    private float LengthFromNode(Vector2 targetPosition)
+    {
+        var length = targetPosition - (Vector2) _previousNode.transform.position;
+
+        return length.sqrMagnitude;
+    }
+
+    private bool OvershotTarget()
+    {
+        var nodeToTarget = LengthFromNode(_targetNode.transform.position);
+        var nodeToSelf = LengthFromNode(transform.localPosition);
+
+        return nodeToSelf > nodeToTarget;
+    }
+
+    //ORIENTATION
     private void UpdateOrientation()
     {
         if (_direction == Vector2.left)
@@ -121,76 +208,7 @@ public class PacmanMove : MonoBehaviour
         }
     }
 
-    private static Node GetNodeAtPosition(Vector2 position)
-    {
-        var tile = GameObject.Find("game").GetComponent<GameBoard>().Board[(int) position.x, (int) position.y];
-
-        return tile != null ? tile.GetComponent<Node>() : null;
-    }
-
-    private Node CanMove(Vector2 direction)
-    {
-        Node moveToNode = null;
-
-        //iterate over neighbours of pellet
-        for (var i = 0; i < _currentNode.neighbours.Length; i++)
-        {
-            //check if current direction of pacman is valid
-            if (_currentNode.validDirections[i] != direction) continue;
-            
-            moveToNode = _currentNode.neighbours[i];
-            
-            break;
-        }
-
-        return moveToNode;
-    }
-
-    private void MoveToNode(Vector2 direction)
-    {
-        var moveToNode = CanMove(direction);
-
-        if (moveToNode == null) return;
-        
-        //change pacman's position if he is able to move
-        transform.localPosition = moveToNode.transform.position;
-        _currentNode = moveToNode;
-    }
-
-    private void ChangePosition(Vector2 targetDirection)
-    {
-        if (targetDirection != _direction)
-        {
-            _nextDirection = targetDirection;
-        }
-
-        if (ReferenceEquals(_currentNode, null)) return;
-        
-        var moveToNode = CanMove(targetDirection);
-
-        if (ReferenceEquals(moveToNode, null)) return;
-            
-        _direction = targetDirection;
-        _targetNode = moveToNode;
-        _previousNode = _currentNode;
-        _currentNode = null;
-    }
-
-    private float LengthFromNode(Vector2 targetPosition)
-    {
-        var length = targetPosition - (Vector2) _previousNode.transform.position;
-
-        return length.sqrMagnitude;
-    }
-
-    private bool OvershotTarget()
-    {
-        var nodeToTarget = LengthFromNode(_targetNode.transform.position);
-        var nodeToSelf = LengthFromNode(transform.localPosition);
-
-        return nodeToSelf > nodeToTarget;
-    }
-
+    //ANIMATION
     private void UpdateAnimation()
     {
         //pacman not moving
@@ -202,6 +220,33 @@ public class PacmanMove : MonoBehaviour
         else
         {
             _animator.enabled = true;
+        }
+    }
+
+    //CONSUME PELLETS
+    private GameObject GetTileAtPosition(Vector2 position)
+    {
+        var tileX = Mathf.RoundToInt(position.x);
+        var tileY = Mathf.RoundToInt(position.y);
+        var tile = _gameBoard.Board[tileX, tileY];
+
+        return tile;
+    }
+
+    private void ConsumePellet()
+    {
+        var tileObject = GetTileAtPosition(transform.position);
+
+        if (ReferenceEquals(tileObject, null)) return;
+        
+        var tile = tileObject.GetComponent<Tile>();
+
+        if (ReferenceEquals(tile, null)) return;
+
+        if (!tile.consumed && (tile.isPellet || tile.isSuperPellet))
+        {
+            tileObject.GetComponent<SpriteRenderer>().enabled = false;
+            tile.consumed = true;
         }
     }
 }
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public bool isPellet;
+    public bool isSuperPellet;
+    public bool consumed;
 }
