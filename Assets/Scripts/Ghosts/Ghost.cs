ec8a22406f81b9ece7a29c47b074c28e686ba7e3
Sian Pike <sian.pike@icloud.com>
2021-04-09 17:44:13 +0100

    Added main menu wip

--- Assets/Scripts/Ghosts/Ghost.cs
+++ Assets/Scripts/Ghosts/Ghost.cs
@@ -1,128 +1,130 @@
-using Ghosts;
 using ScriptResources;
 using UnityEngine;
 
-public class Ghost : MonoBehaviour
+namespace Ghosts
 {
-    [SerializeField] public GhostType ghostType = GhostType.Blinky;
-    [SerializeField] public Node homeNode;
-    [SerializeField] public bool isInGhostHouse;
-    [SerializeField] public RuntimeAnimatorController ghostUp;
-    [SerializeField] public RuntimeAnimatorController ghostDown;
-    [SerializeField] public RuntimeAnimatorController ghostLeft;
-    [SerializeField] public RuntimeAnimatorController ghostRight;
-    [SerializeField] public RuntimeAnimatorController ghostFrightened;
-    [SerializeField] public RuntimeAnimatorController ghostWhite;
-    [SerializeField] public Sprite eyesUp;
-    [SerializeField] public Sprite eyesDown;
-    [SerializeField] public Sprite eyesLeft;
-    [SerializeField] public Sprite eyesRight;
-    [SerializeField] public Node ghostHouse;
+    public class Ghost : MonoBehaviour
+    {
+        [SerializeField] public GhostType ghostType = GhostType.Blinky;
+        [SerializeField] public Node homeNode;
+        [SerializeField] public bool isInGhostHouse;
+        [SerializeField] public RuntimeAnimatorController ghostUp;
+        [SerializeField] public RuntimeAnimatorController ghostDown;
+        [SerializeField] public RuntimeAnimatorController ghostLeft;
+        [SerializeField] public RuntimeAnimatorController ghostRight;
+        [SerializeField] public RuntimeAnimatorController ghostFrightened;
+        [SerializeField] public RuntimeAnimatorController ghostWhite;
+        [SerializeField] public Sprite eyesUp;
+        [SerializeField] public Sprite eyesDown;
+        [SerializeField] public Sprite eyesLeft;
+        [SerializeField] public Sprite eyesRight;
+        [SerializeField] public Node ghostHouse;
 
-    public float ghostReleaseTimer;
-    public int pinkyReleaseTimer = 5;
-    public int inkyReleaseTimer = 14;
-    public int clydeReleaseTimer = 21;
-    public int startBlinkingAt = 7;
-    public float blinkTimer;
-    public bool ghostIsWhite;
-    public Node startingPosition;
-    public bool canMove = true;
+        public float ghostReleaseTimer;
+        public int pinkyReleaseTimer = 5;
+        public int inkyReleaseTimer = 14;
+        public int clydeReleaseTimer = 21;
+        public int startBlinkingAt = 7;
+        public float blinkTimer;
+        public bool ghostIsWhite;
+        public Node startingPosition;
+        public bool canMove = true;
     
-    private GhostMove _ghostMovement;
-    public Vector2 direction = Vector2.zero;
-    private GhostMode _mode;
-    private Collision _collision;
-    private GhostAnimation _ghostAnimation;
-    private GhostOrientation _ghostOrientation;
+        private GhostMove _ghostMovement;
+        public Vector2 direction = Vector2.zero;
+        private GhostMode _mode;
+        private Collision _collision;
+        private GhostAnimation _ghostAnimation;
+        private GhostOrientation _ghostOrientation;
 
-    private void Start()
-    {
-        _ghostMovement = GetComponent<GhostMove>();
-        _mode = GetComponent<GhostMode>();
-        _collision = GetComponent<Collision>();
-        _ghostAnimation = GetComponent<GhostAnimation>();
-        _ghostOrientation = GetComponent<GhostOrientation>();
-    }
-
-    private void Update()
-    {
-        if (canMove)
+        private void Start()
         {
-            _mode.ModeUpdate();
-            direction = _ghostMovement.MoveSprite();
-            _ghostOrientation.UpdateOrientation(direction);
-            _ghostAnimation.UpdateAnimation(direction);
-            _ghostMovement.ReleaseGhosts();
-            _collision.CheckCollision(_mode);
-            IsInGhostHouse();
+            _ghostMovement = GetComponent<GhostMove>();
+            _mode = GetComponent<GhostMode>();
+            _collision = GetComponent<Collision>();
+            _ghostAnimation = GetComponent<GhostAnimation>();
+            _ghostOrientation = GetComponent<GhostOrientation>();
         }
-    }
 
-    private void IsInGhostHouse()
-    {
-        if (_mode.currentMode == Mode.Consumed)
+        private void Update()
         {
-            var tile = _ghostMovement.GetTileAtPosition(transform.position);
-
-            if (tile != null)
+            if (canMove)
             {
-                var tileComponent = tile.transform.GetComponent<Tile>();
+                _mode.ModeUpdate();
+                direction = _ghostMovement.MoveSprite();
+                _ghostOrientation.UpdateOrientation(direction);
+                _ghostAnimation.UpdateAnimation(direction);
+                _ghostMovement.ReleaseGhosts();
+                _collision.CheckCollision(_mode);
+                IsInGhostHouse();
+            }
+        }
 
-                if (tileComponent != null)
+        private void IsInGhostHouse()
+        {
+            if (_mode.currentMode == Mode.Consumed)
+            {
+                var tile = _ghostMovement.GetTileAtPosition(transform.position);
+
+                if (tile != null)
                 {
-                    if (tileComponent.isGhostHouse)
+                    var tileComponent = tile.transform.GetComponent<Tile>();
+
+                    if (tileComponent != null)
                     {
-                        _ghostMovement.speed = _mode.normalSpeed;
-
-                        var node = _ghostMovement.GetNodeAtPosition(transform.position);
-
-                        if (node != null)
+                        if (tileComponent.isGhostHouse)
                         {
-                            _ghostMovement.currentNode = node;
-                            direction = Vector2.up;
-                            _ghostAnimation.UpdateAnimation(direction);
-                            _ghostMovement.targetNode = _ghostMovement.currentNode.neighbours[0];
-                            _ghostMovement.previousNode = _ghostMovement.currentNode;
-                            _mode.currentMode = Mode.Chase;
+                            _ghostMovement.speed = _mode.normalSpeed;
+
+                            var node = _ghostMovement.GetNodeAtPosition(transform.position);
+
+                            if (node != null)
+                            {
+                                _ghostMovement.currentNode = node;
+                                direction = Vector2.up;
+                                _ghostAnimation.UpdateAnimation(direction);
+                                _ghostMovement.targetNode = _ghostMovement.currentNode.neighbours[0];
+                                _ghostMovement.previousNode = _ghostMovement.currentNode;
+                                _mode.currentMode = Mode.Chase;
+                            }
                         }
                     }
                 }
             }
         }
-    }
 
-    public void Restart()
-    {
-        canMove = true;
-        _ghostAnimation.spriteRenderer.enabled = true;
-        _mode.currentMode = Mode.Scatter;
-        _ghostMovement.speed = _mode.normalSpeed;
-        _mode.previousSpeed = 0;
-        transform.position = startingPosition.transform.position;
-        ghostReleaseTimer = 0;
-        _mode.modeChangeIteration = 1;
-        _mode.modeChangeTimer = 0;
-
-        if (transform.name != "blinky")
+        public void Restart()
         {
-            isInGhostHouse = true;
-        }
+            canMove = true;
+            _ghostAnimation.spriteRenderer.enabled = true;
+            _mode.currentMode = Mode.Scatter;
+            _ghostMovement.speed = _mode.normalSpeed;
+            _mode.previousSpeed = 0;
+            transform.position = startingPosition.transform.position;
+            ghostReleaseTimer = 0;
+            _mode.modeChangeIteration = 1;
+            _mode.modeChangeTimer = 0;
 
-        _ghostMovement.currentNode = startingPosition;
+            if (transform.name != "blinky")
+            {
+                isInGhostHouse = true;
+            }
 
-        if (isInGhostHouse)
-        {
-            direction = Vector2.up;
-            _ghostMovement.targetNode = _ghostMovement.currentNode.neighbours[0];
-        }
-        else
-        {
-            direction = Vector2.right;
-            _ghostMovement.targetNode = _ghostMovement.ChooseNextNode();
-        }
+            _ghostMovement.currentNode = startingPosition;
 
-        _ghostMovement.previousNode = _ghostMovement.currentNode;
-        _ghostAnimation.UpdateAnimation(direction);
+            if (isInGhostHouse)
+            {
+                direction = Vector2.up;
+                _ghostMovement.targetNode = _ghostMovement.currentNode.neighbours[0];
+            }
+            else
+            {
+                direction = Vector2.right;
+                _ghostMovement.targetNode = _ghostMovement.ChooseNextNode();
+            }
+
+            _ghostMovement.previousNode = _ghostMovement.currentNode;
+            _ghostAnimation.UpdateAnimation(direction);
+        }
     }
 }
